f93a25015a468a42e156a56669fd60e0ff10220d
Yue Fei <infyniteheap1123@outlook.com>
2025-04-19 08:35:41 +0800

    Improve experience
    Now the non-client area (title bar) and the client area (web page) will be shown simultaneously, just like a native app.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,10 @@
 mod vibrancy;
 
+#[tauri::command]
+fn show_window(window: tauri::Window) {
+    window.show().expect("Failed to show window")
+}
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -11,7 +16,7 @@ pub fn run() {
     tauri::Builder::default()
         .setup(vibrancy::enable_window_vibrancy)
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![show_window, greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
