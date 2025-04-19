55bfc32bea68e7523aced62a65d3c4e5caf34ea7
Yue Fei <infyniteheap1123@outlook.com>
2025-04-19 13:22:52 +0800

    Use trait to guarantee implementations consistent

--- src-tauri/src/vibrancy/macos.rs
+++ src-tauri/src/vibrancy/macos.rs
@@ -1,6 +1,12 @@
+use super::EnableWindowVibrancy;
+
 use tauri::WebviewWindow;
 use window_vibrancy::{Error, NSVisualEffectMaterial};
 
-pub(super) fn enable(ww: &WebviewWindow) -> Result<(), Error> {
-    window_vibrancy::apply_vibrancy(ww, NSVisualEffectMaterial::FullScreenUI, None, None)
+pub(super) struct MacOS;
+
+impl EnableWindowVibrancy for MacOS {
+    fn enable(ww: &WebviewWindow) -> Result<(), Error> {
+        window_vibrancy::apply_vibrancy(ww, NSVisualEffectMaterial::FullScreenUI, None, None)
+    }
 }
